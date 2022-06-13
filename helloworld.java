310a414cb13809c6ca83c0ec93bd1fe5d0641b68
raghuram chilamakuri <chilamakuriraghuram2@gmail.com>
2022-06-13 00:37:36 +0000

    this is my first commit from mobile thank you so much for the founder of code snack

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class helloworld {
+    public static void main(String[]args){
+        System.out.println("this the the way you can write your code from phone and commit it to your repo");    
+    }
+}
